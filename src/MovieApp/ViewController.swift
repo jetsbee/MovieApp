702c20573b3e11fb3bd428c5d0ad601b6b1e4790
Jaeseob Hwang <jetsbee@gmail.com>
2019-09-17 17:47:33 +0900

    Init xcode proj

---
+++ src/MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Jaeseob Hwang on 17/09/2019.
+//  Copyright © 2019 Jaeseob Hwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
