e3c0fe06d925aba64d0a4cdf4d6c1c9b3c51cec2
Jaeseob Hwang <jetsbee@gmail.com>
2019-09-18 13:35:07 +0900

    Added label to MovieListView

--- src/MovieApp/AppDelegate.swift
+++ src/MovieApp/AppDelegate.swift
@@ -14,8 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var appCoordinator: AppCoordinator!
-    //var navigationController: UINavigationController!
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- src/MovieApp/Features/MovieList/View/MovieListViewController.swift
+++ src/MovieApp/Features/MovieList/View/MovieListViewController.swift
@@ -10,12 +10,32 @@ import UIKit
 
 class MovieListViewController: UIViewController {
     
+    let labelView = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any aditional setup after loading the view.
-        print("This is movie list view controller")
+        setupViews()
     }
     
+    fileprivate func setupViews() {
+        labelView.text = "This is movie list view controller"
+        addSubviews()
+        constrainSubviews()
+    }
+    
+    fileprivate func addSubviews() {
+        view.addSubview(labelView)
+    }
+    
+    fileprivate func constrainSubviews() {
+        labelView.textAlignment = .center
+        labelView.translatesAutoresizingMaskIntoConstraints = false
+        labelView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        labelView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        labelView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        labelView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
     
 }
 
