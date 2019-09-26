70691a6d8851b94134f892554be5710eef3141b1
Sangmo Koo <sangmo93@blueprint-lab.com>
2019-09-26 11:41:29 +0900

    Add TitleLabel, IdLabel on MovieDetail View

--- src/MovieApp/Features/MovieDetail/Views/MovieDetailViewController.swift
+++ src/MovieApp/Features/MovieDetail/Views/MovieDetailViewController.swift
@@ -30,12 +30,32 @@ class MovieDetailViewController: UIViewController {
         return backButtonView
     }()
     
+    let TitleLabelView: UILabel = {
+        let TitleLabelView = UILabel()
+        TitleLabelView.font = UIFont.boldSystemFont(ofSize: TitleLabelView.font.pointSize)
+        TitleLabelView.textAlignment = .center
+        TitleLabelView.backgroundColor = .white
+        
+        return TitleLabelView
+    }()
+    
+    let IdLabelView: UILabel = {
+        let IdLabelView = UILabel()
+        IdLabelView.font = UIFont.boldSystemFont(ofSize: IdLabelView.font.pointSize)
+        IdLabelView.textAlignment = .center
+        IdLabelView.backgroundColor = .white
+        
+        return IdLabelView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any aditional setup after loading the view.
         fillUI()
         setupViews()
-        print("Hello, MovieDetailViewController")
+        TitleLabelView.text = "Title: \(viewModel?.detail?.title ?? "")"
+        IdLabelView.text = "Id: \(viewModel?.detail?.id ?? "")"
         print(viewModel?.detail as Any)
         
     }
@@ -53,6 +73,8 @@ class MovieDetailViewController: UIViewController {
         
     fileprivate func addSubviews() {
         view.addSubview(backButtonView)
+        view.addSubview(TitleLabelView)
+        view.addSubview(IdLabelView)
     }
         
     fileprivate func constrainSubviews() {
@@ -61,6 +83,18 @@ class MovieDetailViewController: UIViewController {
         backButtonView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         backButtonView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         backButtonView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        TitleLabelView.translatesAutoresizingMaskIntoConstraints = false
+        TitleLabelView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        TitleLabelView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        TitleLabelView.topAnchor.constraint(equalTo: backButtonView.bottomAnchor).isActive = true
+        TitleLabelView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        IdLabelView.translatesAutoresizingMaskIntoConstraints = false
+        IdLabelView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        IdLabelView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        IdLabelView.topAnchor.constraint(equalTo: TitleLabelView.bottomAnchor).isActive = true
+        IdLabelView.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
     @objc
